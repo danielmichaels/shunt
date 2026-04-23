bd47288e6eb42f8035c9b55a6bf5a68d446cc51b
Daniel Michaels <dan@danielms.site>
2026-04-23 14:16:23 +1000

    fix: add fallback version behaviour for go install
    Signed-off-by: Daniel Michaels <dan@danielms.site>

--- cmd/shunt/main.go
+++ cmd/shunt/main.go
@@ -3,12 +3,46 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/alecthomas/kong"
 	"github.com/danielmichaels/shunt/cmd/shunt/cmd"
 )
 
 var version = "dev"
+
+func resolveVersion() string {
+	if version != "dev" {
+		return version
+	}
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return version
+	}
+	if v := info.Main.Version; v != "" && v != "(devel)" {
+		return v
+	}
+	var revision, modified string
+	for _, s := range info.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			revision = s.Value
+		case "vcs.modified":
+			modified = s.Value
+		}
+	}
+	if revision == "" {
+		return version
+	}
+	if len(revision) > 7 {
+		revision = revision[:7]
+	}
+	v := "dev-" + revision
+	if modified == "true" {
+		v += "+dirty"
+	}
+	return v
+}
 
 type CLI struct {
 	cmd.Globals
@@ -30,15 +64,16 @@ func run() error {
 	if len(os.Args) < 2 {
 		os.Args = append(os.Args, "--help")
 	}
+	v := resolveVersion()
 	ctx := kong.Parse(&cli,
 		kong.Name("shunt"),
 		kong.Description("A rule-based message router for NATS JetStream"),
 		kong.UsageOnError(),
 		kong.ConfigureHelp(kong.HelpOptions{Compact: true}),
 		kong.DefaultEnvars("SHUNT"),
-		kong.Vars{"version": version},
+		kong.Vars{"version": v},
 	)
-	cli.Globals.Version = version
+	cli.Globals.Version = v
 	return ctx.Run(&cli.Globals)
 }
 
